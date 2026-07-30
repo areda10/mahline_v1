9fcb08b41420782e64c6a41175756447c93f45f0
areda10 <aredasri@gmail.com>
2026-07-30 20:11:46 +0000

    update Helpers/helpers.php && app/Core/Providers/CoreServiceProvider.php to lunch the framwork "composer dump-autoload"

--- app/Core/Helpers/helpers.php
+++ app/Core/Helpers/helpers.php
@@ -1,26 +1,3 @@
 <?php
 
 declare(strict_types=1);
-
-namespace App\Core\Providers;
-
-use Illuminate\Support\ServiceProvider;
-
-class CoreServiceProvider extends ServiceProvider
-{
-    /**
-     * Register services.
-     */
-    public function register(): void
-    {
-        //
-    }
-
-    /**
-     * Bootstrap services.
-     */
-    public function boot(): void
-    {
-        //
-    }
-}
