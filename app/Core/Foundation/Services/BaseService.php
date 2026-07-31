531a0fb831389390d597ac69826729b37a7e8adc
areda10 <aredasri@gmail.com>
2026-07-31 02:48:06 +0000

    Creation BaseService mais il est empty pour le moment

---
+++ app/Core/Foundation/Services/BaseService.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Core\Foundation\Services;
+
+/**
+ * Classe de base des services métier MAHLINE.
+ */
+abstract class BaseService
+{
+    // Intentionnellement vide.
+}
