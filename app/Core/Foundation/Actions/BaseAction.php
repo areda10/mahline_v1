73281c4c63b0bbcaea95ca01c64a9a72a97e0c48
areda10 <aredasri@gmail.com>
2026-08-01 18:08:07 +0000

    creation file BaseAction.php

---
+++ app/Core/Foundation/Actions/BaseAction.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Core\Foundation\Actions;
+
+/**
+ * Classe de base de toutes les Actions MAHLINE.
+ *
+ * Une Action représente un cas d'utilisation unique.
+ */
+abstract class BaseAction
+{
+}
