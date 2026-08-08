435c232346744992cab5341bbd72d45c04285af1
areda10 <aredasri@gmail.com>
2026-08-08 03:46:07 +0000

    BaseEnum.php 1-> BaseEnum.php sera un trait, pas une classe. 2 -> BaseEnum utilisera InteractsWithEnum.

---
+++ app/Core/Foundation/Enums/BaseEnum.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Core\Foundation\Enums;
+
+use App\Core\Foundation\Enums\Traits\InteractsWithEnum;
+
+trait BaseEnum
+{
+    use InteractsWithEnum;
+}
