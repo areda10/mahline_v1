74e9b672205e89f173297cc1d259609abf56d97f
areda10 <aredasri@gmail.com>
2026-08-09 19:09:02 +0000

    Model user ok

---
+++ app/Domains/Identity/Users/Models/User.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domains\Identity\Users\Models;
+
+use App\Core\Foundation\Enums\Identity\UserStatus;
+use App\Core\Foundation\Models\BaseModel;
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+final class User extends BaseModel implements AuthenticatableContract
+{
+    use Authenticatable;
+    use SoftDeletes;
+
+    /**
+     * The table associated with the model.
+     */
+    protected $table = 'users';
+
+    /**
+     * The attributes that are mass assignable.
+     */
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'display_name',
+        'email',
+        'telephone',
+        'password',
+        'status',
+        'locale',
+        'timezone',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     */
+    protected function casts(): array
+    {
+        return [
+            'status' => UserStatus::class,
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+            'deleted_at' => 'datetime',
+        ];
+    }
+}
