420536b7e541c41ce660e73d01be5ddd492e4b4a
areda10 <aredasri@gmail.com>
2026-09-03 16:17:01 +0000

    change UserStatus from ==> use App\Core\Foundation\Enums\Identity\UserStatus; ==> to use App\Domains\Identity\Enums\UserStatus;

--- tests/Framework/Domains/Identity/Users/Models/UserTest.php
+++ tests/Framework/Domains/Identity/Users/Models/UserTest.php
@@ -4,8 +4,9 @@ declare(strict_types=1);
 
 namespace Tests\Framework\Domains\Identity\Users\Models;
 
-use App\Core\Foundation\Enums\Identity\UserStatus;
+
 use App\Core\Foundation\Models\BaseModel;
+use App\Domains\Identity\Enums\UserStatus;
 use App\Domains\Identity\Users\Models\User;
 use Illuminate\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\SoftDeletes;
