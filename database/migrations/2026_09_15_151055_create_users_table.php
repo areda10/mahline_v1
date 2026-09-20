55466cd9c7627099f072c7aa6afa35684a3305f6
areda10 <aredasri@gmail.com>
2026-09-20 19:42:38 +0000

    migration create user table delete create token && make another Migration 2026_09_20_192430_create_password_reset_tokens_table

--- database/migrations/2026_09_15_151055_create_users_table.php
+++ database/migrations/2026_09_15_151055_create_users_table.php
@@ -102,18 +102,6 @@ return new class extends Migration
 
         /*
         |--------------------------------------------------------------------------
-        | Password reset tokens
-        |--------------------------------------------------------------------------
-        */
-
-        Schema::create('password_reset_tokens', function (Blueprint $table): void {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
-
-        /*
-        |--------------------------------------------------------------------------
         | Sessions
         |--------------------------------------------------------------------------
         */
@@ -134,7 +122,6 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('sessions');
-        Schema::dropIfExists('password_reset_tokens');
         Schema::dropIfExists('users');
     }
 };
